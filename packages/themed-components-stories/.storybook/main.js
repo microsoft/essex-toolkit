756453b528c84a822cfb49ee8e783023949b433c
Chris Trevino <chtrevin@microsoft.com>
2021-06-07 16:04:45 -0700

    Simplify storybook configuration

--- packages/themed-components-stories/.storybook/main.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-const { configure } = require('@essex/storybook-config/lib/main')
-
-module.exports = configure({ pnp: true })
