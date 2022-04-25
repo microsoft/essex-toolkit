131bfc93ce0abf54c4ca3b1be3706f17924133f1
Chris Trevino <chtrevin@microsoft.com>
2022-04-25 15:32:53 -0700

    add sparkline stories

---
+++ packages/charts-react/src/__tests__/Sparkbar.stories.tsx
@@ -0,0 +1,14 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { Sparkbar } from '../Sparkbar.js'
+
+const meta = {
+	title: 'Sparkbar',
+}
+export default meta
+
+export const Basic = () => {
+	return <Sparkbar width={150} height={30} data={[1, 2, 3, 4]}></Sparkbar>
+}
