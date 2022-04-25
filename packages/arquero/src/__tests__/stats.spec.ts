7077233adc32a3dd5979981b81f6f155663a05fa
Chris Trevino <chtrevin@microsoft.com>
2022-04-25 16:41:27 -0700

    lint fix

--- packages/arquero/src/__tests__/stats.spec.ts
+++ packages/arquero/src/__tests__/stats.spec.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { table } from 'arquero'
 import type ColumnTable from 'arquero/dist/types/table/column-table'
 
