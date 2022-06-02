5566a0f5d7e1b74eeafefae756a07f3026610331
Chris Trevino <darthtrevino@users.noreply.github.com>
2022-06-02 19:11:26 +0000

    use styled-component in container

--- packages/stories/.storybook/ThematicFluentDecorator.tsx
+++ packages/stories/.storybook/ThematicFluentDecorator.tsx
@@ -2,11 +2,9 @@ import { useState, useCallback, useMemo } from 'react'
 import { Toggle } from '@fluentui/react'
 import { ThemeVariant, loadById } from '@thematic/core'
 import { ApplicationStyles } from '@thematic/react'
-import { ThematicFluentProvider, loadFluentTheme } from '@thematic/fluent'
+import { ThematicFluentProvider } from '@thematic/fluent'
 import { StoryFnReactReturnType } from '@storybook/react/dist/ts3.9/client/preview/types'
-import { ThemeProvider as ThemeProviderRaw } from 'styled-components'
-
-const ThemeProvider = ThemeProviderRaw as any
+import styled, { ThemeProvider } from 'styled-components'
 
 /**
  * ThematicFluentDecorator configures both Thematic and the Fluent wrapper
@@ -26,11 +24,6 @@ export const ThematicFluentDecorator = (
 		[dark],
 	)
 
-	const fluentTheme = useMemo(
-		() => loadFluentTheme(thematicTheme),
-		[thematicTheme],
-	)
-
 	const handleDarkChange = useCallback((e, v) => {
 		setDark(v)
 	}, [])
@@ -39,14 +32,12 @@ export const ThematicFluentDecorator = (
 			<ApplicationStyles />
 			<Toggle label="Dark mode" checked={dark} onChange={handleDarkChange} />
 			<ThemeProvider theme={thematicTheme}>
-				<div
-					style={{
-						borderTop: `1px solid ${thematicTheme.application().faint().hex()}`,
-					}}
-				>
-					{storyFn(undefined, undefined)}
-				</div>
+				<Container>{storyFn(undefined, undefined)}</Container>
 			</ThemeProvider>
 		</ThematicFluentProvider>
 	)
 }
+
+const Container = styled.div`
+	border: 1px solid ${({ theme }) => theme.application().faint().hex()};
+`
