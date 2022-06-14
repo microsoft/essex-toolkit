e6e42c4c8e7cd3868f1e21da2c48fe86f06f7969
gaudyb <85708998+gaudyb@users.noreply.github.com>
2022-06-14 20:05:56 +0000

    remove unused code

--- packages/arquero-react/src/ArqueroDetailsList/__tests__/ArqueroDetailsList.stories.tsx
+++ packages/arquero-react/src/ArqueroDetailsList/__tests__/ArqueroDetailsList.stories.tsx
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 import { SortDirection, TableMetadata } from '@essex/arquero'
 import { ArqueroDetailsList, ArqueroTableHeader } from '@essex/arquero-react'
 import { DetailsListLayoutMode, IColumn, SelectionMode } from '@fluentui/react'
