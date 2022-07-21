9348dfe447f46530259d65d3bd30c2573f4b2789
Dayenne <ddesouza@microsoft.com>
2022-07-21 19:47:47 -0300

    fix lint

--- packages/styled-components/src/index.ts
+++ packages/styled-components/src/index.ts
@@ -1,4 +1,8 @@
-import styled, { StyledInterface } from 'styled-components'
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import styled, { type StyledInterface } from 'styled-components'
 
 const defaultStyled: StyledInterface = (styled as any).default ?? styled
 
