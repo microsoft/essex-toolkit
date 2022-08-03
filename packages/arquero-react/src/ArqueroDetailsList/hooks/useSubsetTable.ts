b61588f62f98502b92dbc54764e9b3608f7c6e11
Dayenne Souza <ddesouza@microsoft.com>
2022-08-03 14:30:59 -0300

    fix arquero-react parsing with different delimiter generating columnNames as array (#148)
    * fix parsing with different delimiters
    
    * apply version
    
    * fix comment

--- packages/arquero-react/src/ArqueroDetailsList/hooks/useSubsetTable.ts
+++ packages/arquero-react/src/ArqueroDetailsList/hooks/useSubsetTable.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import type ColumnTable from 'arquero/dist/types/table/column-table'
+import isArray from 'lodash-es/isArray.js'
 import { useMemo } from 'react'
 
 export function useSubsetTable(
@@ -10,13 +11,22 @@ export function useSubsetTable(
 	columns?: string[],
 ): ColumnTable {
 	return useMemo(() => {
+		let existingColumnNames = table.columnNames()
 		if (columns && columns.length > 0) {
 			// for some reason, it updates here first when changing a table.
 			// doing this stops the super error from arquero while the real columns aren't re-rendered
 			const tableColumns = table.columnNames()
-			const existingColumnNames = columns.filter(x => tableColumns.includes(x))
-			return table.select(existingColumnNames)
+			existingColumnNames = columns.filter(x => tableColumns.includes(x))
 		}
-		return table
+
+		// choosing wrong delimiters makes data get wrongly parsed and in some cases
+		// it generates some columnsNames as arrays instead of strings with ,
+		existingColumnNames = existingColumnNames.map(column => {
+			if (isArray(column)) {
+				return column.join(',')
+			}
+			return column
+		})
+		return table.select(existingColumnNames)
 	}, [table, columns])
 }
