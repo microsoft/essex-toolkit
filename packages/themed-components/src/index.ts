69c2f8dcdef9de530593a7754b479f9e26a03036
Dayenne Souza <ddesouza@microsoft.com>
2022-08-09 18:28:59 -0300

    export new button choice group component (#152)
    * export new component
    
    * fix lint

--- packages/themed-components/src/index.ts
+++ packages/themed-components/src/index.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+export * from './ButtonChoiceGroup/index.js'
 export * from './ClippedGraph/index.js'
 export * from './CollapsiblePanel/index.js'
 export * from './ColumnarMenu/index.js'
