ba7ff13aa91fa129c593b447edcfc34537503ab6
Nathan Evans <github@talkswithnumbers.com>
2022-08-11 16:29:11 -0700

    Remove main DWC packages

--- packages/arquero-react/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-export * from './ArqueroDetailsList/index.js'
-export * from './ArqueroTableHeader/index.js'
-export * from './CommandBar/index.js'
-export * from './GroupHeader/index.js'
