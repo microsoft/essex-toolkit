738825d457409706545407805844e5f52230d47d
Nathan Evans <github@talkswithnumbers.com>
2022-08-11 16:55:52 -0700

    Remove last arquero dep/config

--- packages/stories/.storybook/preview.js
+++ packages/stories/.storybook/preview.js
@@ -4,14 +4,5 @@
  */
 import { addDecorator } from '@storybook/react'
 import { ThematicFluentDecorator } from './ThematicFluentDecorator'
-import { loadCSV } from 'arquero'
 
 addDecorator(ThematicFluentDecorator)
-
-export const loaders = [
-	async () => ({
-		stocks: await loadCSV('./data/stocks.csv', {
-			autoMax: 1000000,
-		}),
-	}),
-]
