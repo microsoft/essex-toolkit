62f229d4fba440213a4dd38429379f7b6a03f3a5
Chris Trevino <darthtrevino@gmail.com>
2023-01-31 16:52:19 -0800

    use essex storybook config v7

--- packages/stories/.storybook/main.js
+++ packages/stories/.storybook/main.js
@@ -3,68 +3,9 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import ResolveTypescriptPlugin from 'resolve-typescript-plugin'
+import { configure } from '@essex/storybook-config/main'
 const path = require('path')
 
-const SWC_CONFIG = {
-	sourceMaps: true,
-	jsc: {
-		target: 'es2021',
-		parser: {
-			syntax: 'typescript',
-			tsx: true,
-			decorators: true,
-			dynamicImport: true,
-			importAssertions: true,
-		},
-		experimental: {
-			keepImportAssertions: true,
-		},
-		transform: {
-			react: { runtime: 'automatic', useBuiltins: true },
-		},
-	},
-}
-
-module.exports = {
-	stories: ['../../*/src/**/*.stories.@(mdx|js|jsx|ts|tsx)'],
+module.exports = configure({
 	staticDirs: [path.join(__dirname, '../public')],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-	],
-	framework: {
-		name: '@storybook/react-webpack5',
-		options: {},
-	},
-	typescript: {
-		reactDocgen: 'react-docgen-typescript',
-		reactDocgenTypescriptOptions: {
-			compilerOptions: {
-				allowSyntheticDefaultImports: false,
-				esModuleInterop: false,
-			},
-		},
-	},
-	webpackFinal(config) {
-		config.resolve.plugins = [
-			...(config.resolve.plugins ?? []),
-			new ResolveTypescriptPlugin(),
-		]
-
-		// Swap out babel w/ swc for transpiling app-assets
-		const babelRule = config.module.rules[2]
-		config.module.rules.splice(2, 1, {
-			test: /\.(cjs|mjs|jsx?|cts|mts|tsx?)$/,
-			loader: require.resolve('swc-loader'),
-			options: SWC_CONFIG,
-			include: babelRule.include,
-			exclude: babelRule.exclude,
-		})
-		return config
-	},
-	docs: {
-		autodocs: true,
-	},
-}
+})
--- packages/stories/.storybook/preview.js
+++ packages/stories/.storybook/preview.js
@@ -2,6 +2,4 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { ThematicFluentDecorator } from './ThematicFluentDecorator.js'
-
-export const decorators = [ThematicFluentDecorator]
+export * from '@essex/storybook-config/preview'
