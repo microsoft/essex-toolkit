f9770a77a3a56cb3d016cb2be4d3f2f1e7ddb970
Chris Trevino <darthtrevino@gmail.com>
2023-03-26 20:33:34 -0700

    lint fix

--- packages/graphql-api-commons/src/app/AppBuilder.ts
+++ packages/graphql-api-commons/src/app/AppBuilder.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/consistent-type-imports, @essex/adjacent-await */
 import { ApolloServer } from '@apollo/server'
 import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled'
 import { startStandaloneServer } from '@apollo/server/standalone'
