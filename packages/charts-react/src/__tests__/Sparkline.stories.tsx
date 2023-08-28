10283cd6b2d7a720968c0e132f60f85ec6e86579
Chris Trevino <chtrevin@microsoft.com>
2023-08-28 14:27:23 -0700

    storybook fixes

--- packages/charts-react/src/__tests__/Sparkline.stories.tsx
+++ packages/charts-react/src/__tests__/Sparkline.stories.tsx
@@ -2,9 +2,6 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type { StoryFn } from '@storybook/react'
-
-import type { SparklineProps } from '../Sparkline.js'
 import { Sparkline } from '../Sparkline.js'
 
 const meta = {
--- packages/components/src/FilterTextbox/FilterTextbox.stories.tsx
+++ packages/components/src/FilterTextbox/FilterTextbox.stories.tsx
@@ -2,10 +2,8 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type { StoryFn } from '@storybook/react'
 
 import { FilterTextbox } from './FilterTextbox.js'
-import type { FilterTextboxProps } from './FilterTextbox.types.js'
 
 const meta = {
 	title: '@essex:components/FilterTextbox',
@@ -19,9 +17,4 @@ const meta = {
 
 export default meta
 
-const Template: StoryFn<typeof FilterTextbox> = (args: FilterTextboxProps) => (
-	<FilterTextbox {...args} />
-)
-
-const Primary = Template.bind({})
-Primary.storyName = 'FilterTextbox'
+export const Primary = {}
