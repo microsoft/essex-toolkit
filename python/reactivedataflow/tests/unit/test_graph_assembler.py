52d6b4569d4f02352a92d8c53cf48406bce22ca0
Chris Trevino <chtrevin@microsoft.com>
2024-06-27 15:48:02 -0700

    GraphAssembler -> GraphBuilder
    * Use builder pattern for chained invocations
    * Add edge model to graph, remove NodeInput model.

--- python/reactivedataflow/tests/unit/test_graph_assembler.py
+++ python/reactivedataflow/tests/unit/test_graph_assembler.py
@@ -5,17 +5,12 @@ import pytest
 import reactivex as rx
 
 from reactivedataflow import (
-    GraphAssembler,
+    GraphBuilder,
     Registry,
 )
 from reactivedataflow.constants import default_output
 from reactivedataflow.errors import NodeIdAlreadyExistsError, OutputNotDefinedError
-from reactivedataflow.model import (
-    Graph,
-    Input,
-    InputNode,
-    ProcessingNode,
-)
+from reactivedataflow.model import Edge, Graph, InputNode, Node
 
 from .define_math_ops import define_math_ops
 
@@ -24,8 +19,7 @@ def test_double_add_node_raises_error():
     registry = Registry()
     define_math_ops(registry)
 
-    assembler = GraphAssembler()
-    assembler.add_node("c1", "constant", config={"value": 1})
+    assembler = GraphBuilder().add_node("c1", "constant", config={"value": 1})
     with pytest.raises(NodeIdAlreadyExistsError):
         assembler.add_node("c1", "constant", config={"value": 2})
     assembler.add_node("c1", "constant", config={"value": 2}, override=True)
@@ -36,9 +30,11 @@ def test_double_add_node_raises_error():
 def test_input_bind():
     registry = Registry()
 
-    assembler = GraphAssembler()
-    assembler.add_input("i1")
-    graph = assembler.build(registry=registry, inputs={"i1": rx.just(1)})
+    graph = (
+        GraphBuilder()
+        .add_input("i1")
+        .build(registry=registry, inputs={"i1": rx.just(1)})
+    )
     assert graph.output_value("i1", default_output) == 1
 
 
@@ -46,9 +42,11 @@ def test_simple_graph():
     registry = Registry()
     define_math_ops(registry)
 
-    assembler = GraphAssembler()
-    assembler.add_node("c1", "constant", config={"value": 1})
-    graph = assembler.build(registry=registry)
+    graph = (
+        GraphBuilder()
+        .add_node("c1", "constant", config={"value": 1})
+        .build(registry=registry)
+    )
     assert graph.output_value("c1", default_output) == 1
 
 
@@ -56,20 +54,19 @@ def test_math_op_graph():
     registry = Registry()
     define_math_ops(registry)
 
-    assembler = GraphAssembler()
-    assembler.add_node("c1", "constant", config={"value": 1})
-    assembler.add_node("c3", "constant", config={"value": 3})
-    assembler.add_node(
-        "n1",
-        "add",
-        array_inputs=[
-            Input(node="c1", port=default_output),
-            Input(node="c3"),
-        ],
+    graph = (
+        GraphBuilder()
+        .add_node("c1", "constant", config={"value": 1})
+        .add_node("c3", "constant", config={"value": 3})
+        .add_node("n1", "add")
+        .add_edge(from_node="c1", to_node="n1")
+        .add_edge(from_node="c3", to_node="n1")
+        .build(registry=registry)
     )
 
-    graph = assembler.build(registry=registry)
-    assert graph.output_value("n1", default_output) == 4
+    assert graph.output_value("c1", default_output) == 1
+    assert graph.output_value("c3", default_output) == 3
+    assert graph.output_value("n1") == 4
 
     # Check subscribe output is working
     value = None
@@ -78,13 +75,13 @@ def test_math_op_graph():
         nonlocal value
         value = v
 
-    graph.output("n1", default_output).subscribe(set_value)
+    graph.output("n1").subscribe(set_value)
     assert value == 4
 
 
 def test_input_node():
     registry = Registry()
-    assembler = GraphAssembler()
+    assembler = GraphBuilder()
     assembler.add_input("i")
 
     subject = rx.subject.BehaviorSubject(1)
@@ -123,41 +120,25 @@ def test_graph_assembler():
     #         n3
     #  n2 ----/
     #
-    assembler = GraphAssembler()
-    # Define Input Layer
-    assembler.add_node("c1", "constant", config={"value": 1})
-    assembler.add_node("c3", "constant", config={"value": 3})
-    assembler.add_node("c5", "constant", config={"value": 5})
-
-    # Define Execution Layer
-    assembler.add_node(
-        "n1",
-        "add",
-        array_inputs=[
-            Input(node="c1"),
-            Input(node="c3"),
-        ],
-    )
-    assembler.add_node(
-        "n2",
-        "add",
-        array_inputs=[
-            Input(node="c5"),
-            Input(node="c3"),
-        ],
+    graph = (
+        GraphBuilder()
+        # Define Input Layer
+        .add_node("c1", "constant", config={"value": 1})
+        .add_node("c3", "constant", config={"value": 3})
+        .add_node("c5", "constant", config={"value": 5})
+        # Define Execution Layer
+        .add_node("n1", "add")
+        .add_edge(from_node="c1", to_node="n1")
+        .add_edge(from_node="c3", to_node="n1")
+        .add_node("n2", "add")
+        .add_edge(from_node="c3", to_node="n2")
+        .add_edge(from_node="c5", to_node="n2")
+        .add_node("n3", "multiply")
+        .add_edge(from_node="n1", to_node="n3", to_port="a")
+        .add_edge(from_node="n2", to_node="n3", to_port="b")
+        .build(registry=registry)
     )
 
-    assembler.add_node(
-        "n3",
-        "multiply",
-        named_inputs={
-            "a": Input(node="n1"),
-            "b": Input(node="n2"),
-        },
-    )
-
-    # Build the graph
-    graph = assembler.build(registry=registry)
     assert graph.output_value("c1", default_output) == 1
     assert graph.output_value("c3", default_output) == 3
     assert graph.output_value("c5", default_output) == 5
@@ -171,48 +152,33 @@ def test_graph_assembler_from_schema():
     registry = Registry()
     define_math_ops(registry)
 
-    assembler = GraphAssembler()
+    assembler = GraphBuilder()
     assembler.load(
         Graph(
             inputs=[
                 InputNode(id="i1"),
             ],
             nodes=[
-                ProcessingNode(id="c3", verb="constant", config={"value": 3}),
-                ProcessingNode(id="c5", verb="constant", config={"value": 5}),
-                ProcessingNode(
-                    id="n1",
-                    verb="add",
-                    array_input=[
-                        Input(node="i1"),
-                        Input(node="c3"),
-                    ],
-                ),
-                ProcessingNode(
-                    id="n2",
-                    verb="add",
-                    array_input=[
-                        Input(node="c5"),
-                        Input(node="c3"),
-                    ],
-                ),
-                ProcessingNode(
-                    id="n3",
-                    verb="multiply",
-                    input={
-                        "a": Input(node="n1"),
-                        "b": Input(node="n2"),
-                    },
-                ),
+                Node(id="c3", verb="constant", config={"value": 3}),
+                Node(id="c5", verb="constant", config={"value": 5}),
+                Node(id="n1", verb="add"),
+                Node(id="n2", verb="add"),
+                Node(id="n3", verb="multiply"),
+            ],
+            edges=[
+                # First sum inputs: 1 + 3 = 4
+                Edge(from_node="i1", to_node="n1"),
+                Edge(from_node="c3", to_node="n1"),
+                # Second sum inputs: 5 + 3 = 8
+                Edge(from_node="c3", to_node="n2"),
+                Edge(from_node="c5", to_node="n2"),
+                # Multiply the sums: 4 * 8 = 32
+                Edge(from_node="n1", to_node="n3", to_port="a"),
+                Edge(from_node="n2", to_node="n3", to_port="b"),
             ],
         )
     )
 
     # Build the graph
     graph = assembler.build(registry=registry, inputs={"i1": rx.just(1)})
-    assert graph.output_value("i1", default_output) == 1
-    assert graph.output_value("c3", default_output) == 3
-    assert graph.output_value("c5", default_output) == 5
-    assert graph.output_value("n1", default_output) == 4
-    assert graph.output_value("n2", default_output) == 8
-    assert graph.output_value("n3", default_output) == 32
+    assert graph.output_value("n3") == 32
