06ba871874f2c7792e13e5e785141f668547cc35
Chris Trevino <chtrevin@microsoft.com>
2024-12-09 10:31:37 -0800

    allow injectable azure credential

--- python/fnllm/fnllm/openai/factories/client.py
+++ python/fnllm/fnllm/openai/factories/client.py
@@ -4,7 +4,7 @@
 
 from typing import cast
 
-from openai import AsyncAzureOpenAI, AsyncOpenAI
+from openai import AsyncOpenAI
 
 from fnllm.openai.config import AzureOpenAIConfig, OpenAIConfig, PublicOpenAIConfig
 from fnllm.openai.types.client import OpenAIClient
@@ -13,32 +13,15 @@ from fnllm.openai.types.client import OpenAIClient
 def create_openai_client(config: OpenAIConfig) -> OpenAIClient:
     """Create a new OpenAI client instance."""
     if config.azure:
-        from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+        from .create_azure_openai_client import create_azure_openai_client
 
-        config = cast(AzureOpenAIConfig, config)
+        return create_azure_openai_client(cast(AzureOpenAIConfig, config))
 
-        token_provider = (
-            get_bearer_token_provider(
-                DefaultAzureCredential(), config.cognitive_services_endpoint
-            )
-            if not config.api_key
-            else None
-        )
+    return create_public_openai_client(cast(PublicOpenAIConfig, config))
 
-        return AsyncAzureOpenAI(
-            api_key=config.api_key,
-            azure_ad_token_provider=token_provider,
-            organization=config.organization,
-            # Azure-Specifics
-            api_version=config.api_version,
-            azure_endpoint=config.endpoint,
-            azure_deployment=config.deployment,
-            timeout=config.timeout,
-            max_retries=0,
-        )
 
-    config = cast(PublicOpenAIConfig, config)
-
+def create_public_openai_client(config: PublicOpenAIConfig) -> OpenAIClient:
+    """Create a new OpenAI client instance."""
     return AsyncOpenAI(
         api_key=config.api_key,
         base_url=config.base_url,
