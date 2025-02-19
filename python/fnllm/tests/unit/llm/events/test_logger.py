cd57e7e6634d8b94fa8affbf972bf499f71a8fc3
Brian Caswell <bcaswell@gmail.com>
2025-02-19 12:26:01 -0500

    use module level logger (#370)
    * use module level logger
    
    * initial clear isn't needed
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Brian Caswell <bcaswell@microsoft.com>

--- python/fnllm/tests/unit/llm/events/test_logger.py
+++ python/fnllm/tests/unit/llm/events/test_logger.py
@@ -2,55 +2,59 @@
 
 """Tests for llm.events.logger."""
 
-from logging import Logger
-from unittest.mock import Mock
+from logging import DEBUG, ERROR, WARNING
 
+import pytest
 from fnllm.events.logger import LLMEventsLogger
 from fnllm.limiting.base import Manifest
 from fnllm.types.metrics import LLMMetrics, LLMUsageMetrics
 
 
-async def test_logger_is_called():
-    logger = Mock(spec=Logger)
-    events = LLMEventsLogger(logger)
+def check_records_and_reset(
+    caplog: pytest.LogCaptureFixture, level: int, prefix: str
+) -> None:
+    assert isinstance(caplog.record_tuples, list)
+    assert len(caplog.records) == 1
+    assert caplog.record_tuples[0][1] == level, (
+        f"expected {level} but got {caplog.record_tuples[0][1]}"
+    )
+    assert caplog.record_tuples[0][0] == "fnllm.events.logger", "unexpected logger name"
+    assert caplog.record_tuples[0][2].startswith(prefix), (
+        f"unexpected message: {caplog.record_tuples[0][2]}"
+    )
+    caplog.clear()
 
-    # asserting all events call logger
-    logger.reset_mock()
+
+async def test_logger_is_called(caplog: pytest.LogCaptureFixture):
+    events = LLMEventsLogger()
+    caplog.set_level(DEBUG)
+
     await events.on_error(None, None, {})
-    logger.error.assert_called_once()
+    check_records_and_reset(caplog, ERROR, "unexpected error occurred for arguments")
 
-    logger.reset_mock()
     await events.on_usage(LLMUsageMetrics())
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "LLM usage")
 
-    logger.reset_mock()
     await events.on_limit_acquired(Manifest())
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "limit acquired for request")
 
-    logger.reset_mock()
     await events.on_limit_released(Manifest())
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "limit released for request")
 
-    logger.reset_mock()
     await events.on_post_limit(Manifest())
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "post request limiting triggered")
 
-    logger.reset_mock()
     await events.on_success(LLMMetrics())
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "request succeed with")
 
-    logger.reset_mock()
     await events.on_cache_hit("", "")
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "cache hit for key")
 
-    logger.reset_mock()
     await events.on_cache_miss("", "")
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "cache miss for key")
 
-    logger.reset_mock()
     await events.on_try(1)
-    logger.debug.assert_called_once()
+    check_records_and_reset(caplog, DEBUG, "calling llm, attempt")
 
-    logger.reset_mock()
-    await events.on_retryable_error(Mock(), 1)
-    logger.warning.assert_called_once()
+    await events.on_retryable_error(BaseException(), 1)
+    check_records_and_reset(caplog, WARNING, "retryable error happened on attempt")
