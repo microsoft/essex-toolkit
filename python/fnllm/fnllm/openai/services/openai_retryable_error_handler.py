9e95c86a718805a585b8a86614727aee82b67273
Chris Trevino <chtrevin@microsoft.com>
2025-04-02 19:58:05 -0700

    update sleep detection to use  header

--- python/fnllm/fnllm/openai/services/openai_retryable_error_handler.py
+++ python/fnllm/fnllm/openai/services/openai_retryable_error_handler.py
@@ -5,12 +5,16 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Final
+from typing import TYPE_CHECKING, Final
 
 from openai import APIConnectionError, InternalServerError, RateLimitError
 
 from fnllm.base.services.errors import InvalidLLMResultError
 from fnllm.openai.errors import OpenAINoChoicesAvailableError
+
+if TYPE_CHECKING:
+    from httpx import Response
+
 
 OPENAI_RETRYABLE_ERRORS: Final[list[type[Exception]]] = [
     RateLimitError,
@@ -26,20 +30,9 @@ class OpenAIRetryableErrorHandler:
 
     async def __call__(self, error: BaseException) -> None:
         """Handle the rate limit error."""
-        sleep_recommendation = self._extract_sleep_recommendation(error)
-        if sleep_recommendation > 0:
-            await asyncio.sleep(sleep_recommendation)
-
-    def _extract_sleep_recommendation(self, error: BaseException) -> float:
-        """Extract the sleep time value from a RateLimitError. This is usually only available in Azure."""
-        please_retry_after_msg: Final = "Rate limit is exceeded. Try again in "
-        error_str = str(error)
-
-        if (
-            not isinstance(error, RateLimitError)
-            or please_retry_after_msg not in error_str
-        ):
-            return 0
-
-        # could be second or seconds
-        return int(error_str.split(please_retry_after_msg)[1].split(" second")[0])
+        if hasattr(error, "response"):
+            res: Response = error.response
+            retry_after = res.headers.get("retry-after", None)
+            if retry_after is not None:
+                retry_after = int(retry_after)
+                await asyncio.sleep(retry_after)
