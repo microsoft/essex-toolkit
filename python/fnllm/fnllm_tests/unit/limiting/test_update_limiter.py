d67ca97577692c49ca4b6d5cb8214854c46a3fb8
Chris Trevino <chtrevin@microsoft.com>
2025-05-28 13:51:05 -0700

    add limiter test

---
+++ python/fnllm/fnllm_tests/unit/limiting/test_update_limiter.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2025 Microsoft Corporation.
+
+from aiolimiter import AsyncLimiter
+from fnllm.limiting import LimitReconciliation
+from fnllm.limiting.update_limiter import update_limiter
+
+
+async def test_update_limiter():
+    limiter = AsyncLimiter(1)
+    reconciliation = LimitReconciliation(limit=1234.0, remaining=5678.0)
+    update_limiter(limiter, reconciliation)
+    assert limiter.max_rate == 1234.0
