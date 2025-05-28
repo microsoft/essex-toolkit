5ccb962b96b58f52dd6b996e6a07a005e6bc8f71
Chris Trevino <chtrevin@microsoft.com>
2025-05-28 13:53:14 -0700

    update check

--- python/fnllm/fnllm_tests/unit/limiting/test_update_limiter.py
+++ python/fnllm/fnllm_tests/unit/limiting/test_update_limiter.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2025 Microsoft Corporation.
 
+import asyncio
+
 from aiolimiter import AsyncLimiter
 from fnllm.limiting import LimitReconciliation
 from fnllm.limiting.update_limiter import update_limiter
 
 
 async def test_update_limiter():
+    await asyncio.sleep(0)
     limiter = AsyncLimiter(1)
     reconciliation = LimitReconciliation(limit=1234.0, remaining=5678.0)
     update_limiter(limiter, reconciliation)
